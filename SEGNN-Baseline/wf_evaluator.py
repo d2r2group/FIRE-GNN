8be53c63c347044050874c47931d439f2863b7b3
Claire Schlesinger <esslushy@gmail.com>
2025-08-11 23:10:23 -0400

    uncommented train eval portion

--- SEGNN-Baseline/wf_evaluator.py
+++ SEGNN-Baseline/wf_evaluator.py
@@ -16,44 +16,44 @@ class WFEvaluator(Evaluator):
         return SlabDataset(data, atom_init, lmax_h, radius, max_neighbors, normalize)
 
     def main(self):
-        # total_wf_top_mae = 0
-        # total_wf_top_mse = 0
-        # total_wf_top_mape = 0
-        # total_wf_bot_mae = 0
-        # total_wf_bot_mse = 0
-        # total_wf_bot_mape = 0
-        # num_batches = len(self.train_loader)
+        total_wf_top_mae = 0
+        total_wf_top_mse = 0
+        total_wf_top_mape = 0
+        total_wf_bot_mae = 0
+        total_wf_bot_mse = 0
+        total_wf_bot_mape = 0
+        num_batches = len(self.train_loader)
 
-        # with torch.inference_mode():
-        #     for data in tqdm(self.train_loader, f"train Batch", leave=False):
-        #         data = data.to(self.device)
+        with torch.inference_mode():
+            for data in tqdm(self.train_loader, f"train Batch", leave=False):
+                data = data.to(self.device)
                 
-        #         wf_top_pred, wf_bot_pred = self.model(data)
+                wf_top_pred, wf_bot_pred = self.model(data)
 
-        #         # Scale loss if normalized
-        #         if self.config["normalize"]:
-        #             wf_top_pred, wf_bot_pred = self.unnormalize(wf_top_pred, wf_bot_pred)
-        #             wf_top_true, wf_bot_true = self.unnormalize(data.wf_top, data.wf_bot)
-        #         else:
-        #             wf_top_true = data.wf_top
-        #             wf_bot_true = data.wf_bot
+                # Scale loss if normalized
+                if self.config["normalize"]:
+                    wf_top_pred, wf_bot_pred = self.unnormalize(wf_top_pred, wf_bot_pred)
+                    wf_top_true, wf_bot_true = self.unnormalize(data.wf_top, data.wf_bot)
+                else:
+                    wf_top_true = data.wf_top
+                    wf_bot_true = data.wf_bot
 
-        #         # Deal with accuracy
-        #         total_wf_top_mae += l1_loss(wf_top_pred, wf_top_true).item()
-        #         total_wf_top_mse += mse_loss(wf_top_pred, wf_top_true).item()
-        #         total_wf_top_mape += mean_absolute_percentage_error(wf_top_pred, wf_top_true).item()
-        #         total_wf_bot_mae += l1_loss(wf_bot_pred, wf_bot_true).item()
-        #         total_wf_bot_mse += mse_loss(wf_bot_pred, wf_bot_true).item()
-        #         total_wf_bot_mape += mean_absolute_percentage_error(wf_bot_pred, wf_bot_true).item()
+                # Deal with accuracy
+                total_wf_top_mae += l1_loss(wf_top_pred, wf_top_true).item()
+                total_wf_top_mse += mse_loss(wf_top_pred, wf_top_true).item()
+                total_wf_top_mape += mean_absolute_percentage_error(wf_top_pred, wf_top_true).item()
+                total_wf_bot_mae += l1_loss(wf_bot_pred, wf_bot_true).item()
+                total_wf_bot_mse += mse_loss(wf_bot_pred, wf_bot_true).item()
+                total_wf_bot_mape += mean_absolute_percentage_error(wf_bot_pred, wf_bot_true).item()
             
-        # print({
-        #     f"train/WF Top MAE": total_wf_top_mae / num_batches,
-        #     f"train/WF Top RMSE": (total_wf_top_mse / num_batches)**(1/2),
-        #     f"train/WF Top MAPE": total_wf_top_mape / num_batches,
-        #     f"train/WF Bottom MAE": total_wf_top_mae / num_batches,
-        #     f"train/WF Bottom RMSE": (total_wf_top_mse / num_batches)**(1/2),
-        #     f"train/WF Bottom MAPE": total_wf_top_mape / num_batches
-        # })
+        print({
+            f"train/WF Top MAE": total_wf_top_mae / num_batches,
+            f"train/WF Top RMSE": (total_wf_top_mse / num_batches)**(1/2),
+            f"train/WF Top MAPE": total_wf_top_mape / num_batches,
+            f"train/WF Bottom MAE": total_wf_bot_mae / num_batches,
+            f"train/WF Bottom RMSE": (total_wf_bot_mse / num_batches)**(1/2),
+            f"train/WF Bottom MAPE": total_wf_bot_mape / num_batches
+        })
 
         total_wf_top_mae = 0
         total_wf_top_mse = 0
